6a6646d1fd0cd19f0b08bfadbe0702d9f36c7765
Juan Pablo <nexxu.mx@gmail.com>
2025-08-29 12:07:00 -0600

    publickt

--- mp3.js
+++ mp3.js
@@ -1,6 +1,6 @@
 console.log('inicia SDK.');
 //PR::: APP_USR-51df598b-b434-4aee-9f64-d4f2365eac7c TEST::: TEST-df88f0fd-9bd4-4762-8e08-9814912fc5a2
-const mp = new MercadoPago("TEST-687bfb98-2fd2-4306-8651-82d5b3b17f25", {
+const mp = new MercadoPago("APP_USR-51df598b-b434-4aee-9f64-d4f2365eac7c", {
     locale: "es-MX"
 });
 
