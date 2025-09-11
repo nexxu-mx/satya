ed7c584d1de4d2610e20146a1cededda73c25e35
Juan Pablo <nexxu.mx@gmail.com>
2025-09-11 08:36:27 -0600

    dok

--- process_payment.php
+++ process_payment.php
@@ -10,6 +10,7 @@ header('Content-Type: application/json');
 // Obtener datos del request
 $data = json_decode(file_get_contents('php://input'), true);
 var_dump($data);
+exit;
 include './db.php';
 include './error_log.php';
 
