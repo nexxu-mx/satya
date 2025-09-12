74915c76e241e5f76652226626eae07bfc002054
Juan Pablo <nexxu.mx@gmail.com>
2025-09-12 10:35:04 -0600

    index

--- index.php
+++ index.php
@@ -19,8 +19,13 @@ include './db.php';
     href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;700&family=Rubik:wght@400;500;700&display=swap"
     rel="stylesheet">
   <link rel="stylesheet" href="./assets/css/style.css?v=<?php echo time(); ?>">
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+   <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+  <!-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"> -->
+  <!-- <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> -->
+   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <?php include 'head.php'; ?>
   <style>
     .swiper-button-next{
