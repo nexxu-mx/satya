c497d3f588c14b8ef803f4bda8af3b1f703bd18a
Juan Pablo <nexxu.mx@gmail.com>
2025-10-02 18:37:46 -0600

    coaches img

--- coaches.php
+++ coaches.php
@@ -83,12 +83,12 @@ session_start();
                                                 ? $idDisciplinas[$fila['id_disciplina']] 
                                                 : 'SATYA';
                                 $coachPath = "./assets/images/coaches/pro/" . $fila['id'] . ".mp4";
-                                $defaultPath = "./assets/images/coaches/pro/" . $fila['id'] . "png?=" . time();
+                                $defaultPath = "./assets/images/coaches/pro/" . $fila['id'] . "png";
                         
                                 if (!file_exists($coachPath)) {
-                                    $imgC = '<img src="'. $defaultPath .'" alt="">';
+                                    $imgC = '<img src="'. $defaultPath .'?v' . time() . '" alt="SATYA - '. $fila['nombre_disciplina'] .'">';
                                     if (!file_exists($defaultPath)) {
-                                        $imgC = '<img src="./assets/images/coaches/pro/unknow.jpg" alt="">';
+                                        $imgC = '<img src="./assets/images/coaches/pro/unknow.jpg" alt="SATYA - '. $fila['nombre_disciplina'] .'">';
                                     }
 
                                 }else{
