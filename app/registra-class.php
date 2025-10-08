216c62fdfd73217ddf87405cbc4e7e22cb7fedea
Juan Pablo <nexxu.mx@gmail.com>
2025-10-07 19:02:09 -0600

    ok

--- app/registra-class.php
+++ app/registra-class.php
@@ -113,7 +113,7 @@ if((int)$_SESSION['tipoUser'] !== 3 && (int)$_SESSION['tipoUser'] !== 4){
                                                     <input type="text" class="form-control" id="duracion" name="duracion" readonly aria-describedby="basic-addon3">
                                                 </div>
                                                 <div class="input-group mb-3">
-                                                    <span class="input-group-text" id="basic-addon3">No Invitados</span>
+                                                    <span class="input-group-text" id="basic-addon3">Lugar Reservado</span>
                                                     <input type="text" class="form-control" id="invitados" name="invitados" readonly aria-describedby="basic-addon3">
                                                 </div>
                                                
