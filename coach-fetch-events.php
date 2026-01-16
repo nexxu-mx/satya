83b993859e5c3d372ddd4dc1d56c6bb597bfc72a
Eduardo Cervantes Jaramillo <luis_edu_cj@hotmail.com>
2026-01-16 14:31:44 -0600

    ticket corregido

--- coach-fetch-events.php
+++ coach-fetch-events.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
 }
 
 session_start();
-$id = $_SESSION['coach']; 
+$id = $_SESSION['coach'];
 date_default_timezone_set('America/Mexico_City');
 
 // Obtener y formatear los parámetros de la URL
@@ -20,10 +20,11 @@ if (!$start || !$end) {
   exit;
 }
 
-function generarToken16Digitos() {
+function generarToken16Digitos()
+{
   $token = '';
   for ($i = 0; $i < 32; $i++) {
-      $token .= random_int(0, 9);
+    $token .= random_int(0, 9);
   }
   return $token;
 }
@@ -42,47 +43,48 @@ $eventos = [];
 while ($row = $result->fetch_assoc()) {
   $token = generarToken16Digitos();
 
-  $cancelable = (time() - $row["hora_inicio"]) > 7200 ? false : true;
+  $timestamp_inicio = strtotime($row["hora_inicio"]);
+  $cancelable = (time() - $timestamp_inicio) > 7200 ? false : true;
   $sqlF = ("SELECT nombre_disciplina FROM disciplinas WHERE id = ?");
   $stmtF = $conn->prepare($sqlF);
   $stmtF->bind_param("i", $row["id_disciplina"]);
   $stmtF->execute();
   $resultF = $stmtF->get_result();
-  if($rowF = $resultF->fetch_assoc()){
-    $n_disciplina = $rowF['nombre_disciplina']; 
-  }else{
+  if ($rowF = $resultF->fetch_assoc()) {
+    $n_disciplina = $rowF['nombre_disciplina'];
+  } else {
     $n_disciplina = "-";
   }
 
 
   $aforo = $row['reservados'] . "/" . $row['aforo'];
- //CONSULTA DURACION DE LA CLASE
-      $hora_inicio = $row['hora_inicio'];
-      $hora_fin = $row['hora_fin'];   
+  //CONSULTA DURACION DE LA CLASE
+  $hora_inicio = $row['hora_inicio'];
+  $hora_fin = $row['hora_fin'];
 
-      $inicio = new DateTime($hora_inicio);
-      $fin = new DateTime($hora_fin);
+  $inicio = new DateTime($hora_inicio);
+  $fin = new DateTime($hora_fin);
 
-      // Calcula la diferencia
-      $intervalo = $inicio->diff($fin);
+  // Calcula la diferencia
+  $intervalo = $inicio->diff($fin);
 
-      // Convierte todo a minutos
-      $diferencia_minutos = ($intervalo->h * 60) + $intervalo->i;
+  // Convierte todo a minutos
+  $diferencia_minutos = ($intervalo->h * 60) + $intervalo->i;
 
-      // Guarda en formato "50 min"
-      $dura = $diferencia_minutos . " min";
+  // Guarda en formato "50 min"
+  $dura = $diferencia_minutos . " min";
 
 
   $name_coach = $_SESSION['nombre'];
- 
+
   $estatus = '<svg xmlns="http://www.w3.org/2000/svg" class="ionicon clase-en-curso-punto" viewBox="0 0 512 512">
                 <defs><style>.ionicon { fill: #986C5D; }</style></defs>
                 <title>Ellipse</title>
                 <path d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z"></path>
-              </svg>'; 
+              </svg>';
 
-              // lista de alumnos
-                $sqlA = "SELECT 
+  // lista de alumnos
+  $sqlA = "SELECT 
                             users.nombre, 
                             reservaciones.id AS reservacion_id, 
                             reservaciones.alumno,
@@ -93,42 +95,42 @@ while ($row = $result->fetch_assoc()) {
                         INNER JOIN users ON reservaciones.alumno = users.id 
                         WHERE reservaciones.idClase = ?";
 
-                $stmtA = $conn->prepare($sqlA);
-                $stmtA->bind_param("i", $row['id']);
-                $stmtA->execute();
-                $resultA = $stmtA->get_result();
-                // Imprimir lista
-                $alumnos = '<ul style="width: 100%;">';
-              
-                while ($rowA = $resultA->fetch_assoc()) {
-                    $name = htmlspecialchars($rowA['nombre']);
-                    $idEvent = $row['id'];
-                    $Ndisciplina = "'$disciplina'";
-                    $a1 = $rowA['reservacion_id'];
-                    $a2 = $rowA['alumno'];
-                    $a3 = $rowA['invitado'];
-                    $a4 = $rowA['lugar'];
-                    if(!$a4 == null){
-                        $lugar = "(Lugar: $a4)";
-                    }else{
-                        $lugar = "";
-                    }
-                
+  $stmtA = $conn->prepare($sqlA);
+  $stmtA->bind_param("i", $row['id']);
+  $stmtA->execute();
+  $resultA = $stmtA->get_result();
+  // Imprimir lista
+  $alumnos = '<ul style="width: 100%;">';
 
-                    $asistencia = 1 + $rowA['invitado'];
+  while ($rowA = $resultA->fetch_assoc()) {
+    $name = htmlspecialchars($rowA['nombre']);
+    $idEvent = $row['id'];
+    $Ndisciplina = "'" . $n_disciplina . "'";
+    $a1 = $rowA['reservacion_id'];
+    $a2 = $rowA['alumno'];
+    $a3 = $rowA['invitado'];
+    $a4 = $rowA['lugar'];
+    if (!$a4 == null) {
+      $lugar = "(Lugar: $a4)";
+    } else {
+      $lugar = "";
+    }
 
-                    if($rowA['notas'] !== null){
-                      $nuser = $rowA['notas'];
-                      $nota = "notausuario('$nuser')";
-                      $iconota = '<button onclick="' . $nota . '"><ion-icon name="information-circle-outline" style="font-size: 2.5rem;color: var(--c2);" aria-hidden="true"></ion-icon></button>';
-                    }else{
-                      $iconota = "";
-                    }
-                    $alumnos.= '<li style="display: flex;justify-content: space-between; align-items: center;"><p>' . $name . ' (x' . $asistencia . ')' . $lugar . '</p><div style="display: flex;gap: 10px;">' . $iconota . '</div></li>';
-                }
-                $alumnos .= "</ul>";
-    
-                
+
+    $asistencia = 1 + $rowA['invitado'];
+
+    if ($rowA['notas'] !== null) {
+      $nuser = $rowA['notas'];
+      $nota = "notausuario('$nuser')";
+      $iconota = '<button onclick="' . $nota . '"><ion-icon name="information-circle-outline" style="font-size: 2.5rem;color: var(--c2);" aria-hidden="true"></ion-icon></button>';
+    } else {
+      $iconota = "";
+    }
+    $alumnos .= '<li style="display: flex;justify-content: space-between; align-items: center;"><p>' . $name . ' (x' . $asistencia . ')' . $lugar . '</p><div style="display: flex;gap: 10px;">' . $iconota . '</div></li>';
+  }
+  $alumnos .= "</ul>";
+
+
   $eventos[] = [
     "id" => $row["id"],
     "title" => $n_disciplina,
@@ -146,4 +148,3 @@ while ($row = $result->fetch_assoc()) {
 
 header('Content-Type: application/json');
 echo json_encode($eventos);
-?>
